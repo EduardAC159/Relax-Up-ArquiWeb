9d3a6a4a2df9c770c4d80786ea1c1865e65760f8
EduardAC159 <eduard_somos@hotmail.com>
2026-04-27 13:10:24 -0500

    crud Contacto Emergencia

---
+++ RelaxUp/src/main/java/pe/edu/upc/relaxup/Dtos/ContactoEmergenciaDTO.java
@@ -0,0 +1,10 @@
+package pe.edu.upc.relaxup.Dtos;
+import pe.edu.upc.relaxup.Entities.Usuario;
+
+public class ContactoEmergenciaDTO {
+    private int idContacto;
+    private String nombre;
+    private int celular;
+    private String relacion;
+    private int idUsuario;
+}
