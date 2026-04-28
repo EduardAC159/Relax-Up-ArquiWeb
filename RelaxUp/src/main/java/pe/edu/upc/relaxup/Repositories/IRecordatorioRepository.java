fedaa385c2c2d7188b595042fb7f1d27d8e82d0f
EduardAC159 <eduard_somos@hotmail.com>
2026-04-28 11:16:55 -0500

    crud recordatorio

--- RelaxUp/src/main/java/pe/edu/upc/relaxup/Repositories/IRecordatorioRepository.java
+++ RelaxUp/src/main/java/pe/edu/upc/relaxup/Repositories/IRecordatorioRepository.java
@@ -2,7 +2,7 @@ package pe.edu.upc.relaxup.Repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import pe.edu.upc.relaxup.Entities.Interaccion;
+import pe.edu.upc.relaxup.Entities.Recordatorio;
 @Repository
-public interface IInteraccionRepository extends JpaRepository<Interaccion, Integer> {
+public interface IRecordatorioRepository extends JpaRepository<Recordatorio,Integer> {
 }
